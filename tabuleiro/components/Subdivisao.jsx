d0883223ce99bd39be1d9b7f28703bb6712ef32e
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-05 08:39:27 -0300

    Projeto tabuleiro #03

---
+++ tabuleiro/components/Subdivisao.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Subdivisao.module.css';
+
+export default function Subdivisao(props){
+  return (
+    <div className={styles.subdivisao}>
+
+    </div>
+  )
+}
---
+++ tabuleiro/pages/index.jsx
@@ -0,0 +1,10 @@
+import Subdivisao from '../components/Subdivisao'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Subdivisao />
+    </div>
+  )
+}
