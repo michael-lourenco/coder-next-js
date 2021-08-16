70f3622503a3507479f0a884eef87c5967708ff8
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-16 12:11:04 -0300

    Navegacao entre Páginas #02

--- next-basico/src/pages/rotas/index.jsx
+++ next-basico/src/pages/rotas/index.jsx
@@ -1,6 +1,20 @@
 import Link from 'next/link'
+import router, { useRouter } from 'next/router'
 
 export default function rotas() {
+  function navegacaoSimples(url){
+    router.push(url)
+  }
+
+  function navegacaoComParams(){
+    router.push({
+      pathname:'/rotas/params',
+      query:{
+        id: 123,
+        nome: 'Josefa',
+      }
+    })
+  }
   return (
     <div>
       <h1>
@@ -17,6 +31,25 @@ export default function rotas() {
           <li>Daniel</li>
         </Link>
       </ul>
+      <div style = {{
+        display: 'flex',
+        flexDirection:'column',
+        alignItems:'flex-start',
+      }}>
+        <button 
+          onClick={ 
+            () => router.push("/rotas/123/buscar")} >
+              Buscar
+        </button>
+        <button 
+          onClick={ 
+            () => navegacaoSimples("/rotas/123/Daniel")} >
+              Daniel
+        </button>
+        <button onClick={ navegacaoComParams } >
+          Params
+        </button>
+      </div>
     </div>
   )
 }
