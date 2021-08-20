32dc0ae5696ac714ea2abafc555a49f670d212b7
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-20 15:23:02 -0300

    Finalizando o formulario

---
+++ portas/components/EntradaNumerica.tsx
@@ -0,0 +1,24 @@
+
+import styles from '../styles/EntradaNumerica.module.css'
+
+interface EntradaNumericaProps {
+  text: string
+  value: number
+  onChange: (newValue: number) => void
+}
+
+export default function EntradaNumerica(props) {
+  const decrementar = () => props.onChange(props.value - 1);
+  const incrementar = () => props.onChange(props.value + 1);
+
+  return (
+    <div className = { styles.entradaNumerica }>
+      <span className = { styles.text }> { props.text }</span>
+      <span className = { styles.value }> { props.value }</span> 
+      <div className = { styles.botoes}>
+        <button className = { styles.btn } onClick = { decrementar } > - </button>
+        <button className = { styles.btn } onClick = { incrementar } > + </button>
+      </div>
+    </div>
+  )
+}
--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,8 +1,13 @@
 import styles from '../styles/Formulario.module.css'
-import Cartao from "../components/Cartao"
+import Cartao from '../components/Cartao'
 import Link from 'next/link'
+import EntradaNumerica from '../components/EntradaNumerica'
+import { useState } from 'react'
 
 export default function Formulario() {
+  const [quantidadePortas, setQuantidadePortas] = useState(3)
+  const [portaComPresente, setPortaComPresente] = useState(2)
+
   return (
     <div className = { styles.formulario } >
       <div>
@@ -11,12 +16,26 @@ export default function Formulario() {
             Monty Roll
           </h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica 
+            text = "Qtd Portas" 
+            value = { quantidadePortas } 
+            onChange = { 
+              novaQuantidade => setQuantidadePortas(novaQuantidade) } 
+          />
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica 
+            text = "Porta Com Presente" 
+            value = { portaComPresente } 
+            onChange = { 
+              novaPortaComPresente => setPortaComPresente(novaPortaComPresente) } 
+          />
+        </Cartao>
         <Cartao bgcolor = "#28a085">
-          <Link href = { `/jogo/4/2` }>
+          <Link href = { `/jogo/${ quantidadePortas }/${ portaComPresente }` }>
             <h2 className = { styles.link }>Iniciar</h2>
           </Link>
         </Cartao>
