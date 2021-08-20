68322d5463993b724ffff817a30872b37105c02e
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-20 11:28:58 -0300

    Estruturando o formulario

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import styles from '../styles/Formulario.module.css'
+import Cartao from "../components/Cartao"
+import Link from 'next/link'
+
+export default function Formulario() {
   return (
-    <div style = {{ display: 'flex', }}>
-      <h1> Formulário de Início!!! </h1>
+    <div className = { styles.formulario } >
+      <div>
+        <Cartao bgcolor = "#c0392c">
+          <h1>
+            Monty Roll
+          </h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor = "#28a085">
+          <Link href = { `/jogo/4/2` }>
+            <h2 className = { styles.link }>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }
