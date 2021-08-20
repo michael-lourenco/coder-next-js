0a5c500bdd25022c35c4ca705f2a503e7ceabf75
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-20 09:27:06 -0300

    Componente jogo #01

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,23 +1,7 @@
-import Porta from "../components/Porta"
-import { useState } from "react"
-import { atualizarPortas, criarPortas } from "../functions/portas";
-
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2));
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return (<Porta 
-        key = { porta.numero }
-        value = { porta } 
-        onChange = { novaPorta => setPortas(atualizarPortas(portas, novaPorta)) } 
-      />)
-    })
-  }
-
   return (
     <div style = {{ display: 'flex', }}>
-      { renderizarPortas() }
+      <h1> Formulário de Início!!! </h1>
     </div>
   )
 }
---
+++ portas/pages/jogo.tsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Jogo.module.css'
+import Porta from "../components/Porta"
+import { useState } from "react"
+import { atualizarPortas, criarPortas } from "../functions/portas";
+
+export default function Jogo() {
+  const [portas, setPortas] = useState(criarPortas(3, 2));
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return (
+        <Porta 
+          key = { porta.numero }
+          value = { porta } 
+          onChange = { novaPorta => setPortas(atualizarPortas(portas, novaPorta)) } 
+        />
+      )
+    })
+  }
+
+  return (
+    <div id = { styles.jogo }>
+      <div className = { styles.portas }>
+        { renderizarPortas() }
+      </div>
+      <div className = { styles.botoes }></div>
+    </div>
+  )
+}
