49fbacb7a6100c93d22a34cfd435349e7d12035c
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-23 16:47:46 -0300

    Estatico #01

---
+++ renderizacao/pages/estatico1.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+export default function Estatico1(){
+  // const [numero, setNumero] = useState(0)
+
+  // useEffect(() => {
+  //   setNumero(Math.random())
+  // }, [])
+
+  return (
+    <div>
+      <h1> Estático #01 </h1>
+      {/*<h2> { numero } </h2>*/}
+    </div>
+  )
+}
