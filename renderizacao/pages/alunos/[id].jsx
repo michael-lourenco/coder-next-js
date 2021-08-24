076074d67fcdbb6134b67b1cb18de214d62480a2
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-24 10:15:09 -0300

    Usando getStaticPaths - Parte 2

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -1,23 +1,40 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const resp = await fetch(`http://localhost:3000/api/alunos/tutores`)
+  const ids = await resp.json()
+
+  const paths = ids.map( id => {
+    return { params: { id: `${ id }` } }
+  })
+
   return {
     fallback: false, // 404 quando nao encontrar o id especifico
-    paths: [
-      {params: { id: '107' }},
-      {params: { id: '203' }},
-      {params: { id: '13343' }},
-    ]
-  }
-}
-export function getStaticProps() {
-  return {
-    props: {},
+    paths
   }
 }
 
-export default function AlunoPorId() {
+export async function getStaticProps({ params }) {
+  const resp = await fetch(`http://localhost:3000/api/alunos/${ params.id }`)
+  const aluno = await resp.json()
+
+  return {
+    props: {
+      aluno
+    },
+  }
+}
+
+export default function AlunoPorId(props) {
+
+  const { aluno } = props
+
   return(
     <div>
       <h1>Detalhes do Aluno</h1>
+      <ul>
+        <li> { aluno.id } </li>
+        <li> { aluno.nome } </li>
+        <li> { aluno.email } </li>
+      </ul>
     </div>
   )
 }
