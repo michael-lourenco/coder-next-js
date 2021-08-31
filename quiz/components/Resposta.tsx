cbb344f04bd2e11b6ba6c44e0ad15bdc32e378fa
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-31 10:21:29 -0300

    Componente resposta #07

--- quiz/components/Resposta.tsx
+++ quiz/components/Resposta.tsx
@@ -15,18 +15,40 @@ export default function Resposta(props: RespostaProps) {
     <div className = { styles.resposta }
       onClick = { () => props.respostaFornecida(props.indice) }>
       <div className = { styles.conteudoResposta }>
+        {!resposta.revelada ? (
         <div className = { styles.frente }>
-          <div className = { styles.letra }
-            style = {{ backgroundColor: props.corFundoLetra }}>
-            { props.letra }
-          </div>
-          <div className = { styles.valor }>
-            { resposta.valor }
-          </div>
+        <div className = { styles.letra }
+          style = {{ backgroundColor: props.corFundoLetra }}>
+          { props.letra }
         </div>
-        <div className = { styles.verso }>
-
+        <div className = { styles.valor }>
+          { resposta.valor }
         </div>
+      </div>
+        ) : (
+          <div className = { styles.verso }>
+          { resposta.certa ? (
+              <div className = { styles.certa }>
+                <div>
+                  A resposta certa é...
+                </div>
+                <div className = { styles.valor }>
+                  { resposta.valor }
+                </div>
+              </div>
+            ) : (
+              <div className = { styles.errada }>
+                <div>
+                  A resposta informada está errada
+                </div>
+                <div className = { styles.valor }>
+                  { resposta.valor }
+                </div>
+              </div>
+            )
+          }
+        </div>
+        )}
       </div>
     </div>
   )
