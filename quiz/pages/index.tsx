560b916d0774a21e4a82cfdf10d8d51b372d2485
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-31 14:04:00 -0300

    Componente index #01

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Questionario from '../components/Questionario'
 import Botao from '../components/Botao'
 import QuestaoModel from '../model/questao'
@@ -11,8 +11,33 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock)
+
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${ BASE_URL }/questionario`)
+    const idsDasQuestoes = await resp.json()
+
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${ BASE_URL }/questoes/${ idQuestao }`)
+    const json = await resp.json()
+
+    console.log(json)
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
 
   function questaoRespondida(questao: QuestaoModel) {
 
@@ -23,19 +48,11 @@ export default function Home() {
   }
 
   return (
-  <div style = {{
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh'
-  }}>
     <Questionario
       questao = { questao }
       ultima = { true }
       questaoRespondida = { questaoRespondida }
       irParaProximoPasso = { irParaProximoPasso }
     />
-  </div>
   )
 }
