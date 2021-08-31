48a9231ddf6ae3fcace7ddc3f0bc0172289e4ad4
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-08-31 09:15:55 -0300

    Componente resposta #01

--- quiz/components/Questao.tsx
+++ quiz/components/Questao.tsx
@@ -1,16 +1,32 @@
 import styles from '../styles/Questao.module.css'
 import QuestaoModel from '../model/questao'
 import Enunciado from './Enunciado'
+import Resposta from './Resposta'
 interface QuestaoProps {
   valor: QuestaoModel
 }
 
-export default function Questao(props) {
+export default function Questao(props: QuestaoProps) {
  const questao = props.valor
+
+ function renderizarRespostas() {
+  return questao.respostas.map((resposta, i) => {
+    return( 
+      <Resposta 
+        key = { i }
+        valor = { resposta }
+        indice = { i }
+        letra = "A"
+        corLetra = "#f2c866"
+      />
+    )
+  })
+ }
 
  return (
    <div className = { styles.questao }>
      <Enunciado texto = { questao.enunciado } />
+     { renderizarRespostas() }
    </div>
  )
 }
