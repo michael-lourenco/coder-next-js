658f1d4ff17c21f5ed5cb3d4ed2cc86d15482f71
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-09-01 10:25:57 -0300

    Componente estatistica

--- quiz/pages/resultado.tsx
+++ quiz/pages/resultado.tsx
@@ -1,5 +1,7 @@
 import styles from '../styles/Resultado.module.css'
 import  { useRouter } from 'next/router'
+import Botao from '../components/Botao'
+import Estatistica from '../components/Estatistica'
 
 export default function Resultado(){
   const router = useRouter();
@@ -10,9 +12,20 @@ export default function Resultado(){
   return (
     <div className= { styles.resultado }>
       <h1>Resultado Final</h1>
-      <div>{ total }</div>
-      <div>{ certas }</div>
-      <div>{ `${ percentual }%` }</div>
+      <div style= {{ display: 'flex',}}>
+        <Estatistica texto = "Perguntas" valor = { total }/>
+        <Estatistica 
+          texto = "Certas" 
+          valor = { certas }
+          corFundo = "#9cd2a4"
+        />
+        <Estatistica 
+          texto = "Percentual" 
+          valor = { `${ percentual }%` }
+          corFundo = "#de6a33"
+        />
+      </div>
+      <Botao href="/" texto="Tentar Novamente" />
     </div>
   )
 }
