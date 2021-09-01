e2b78afad1410cb4038c7202e5e350368cadc97b
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-09-01 17:35:49 -0300

    Criando o projeto

---
+++ tarefas/src/pages/index.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex 
+      flex-col
+      justify-center
+      items-center
+      text-white
+      bg-gradient-to-tr
+      from-purple-500
+      to-yellow-600
+      h-screen
+    `}>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  )
+}
