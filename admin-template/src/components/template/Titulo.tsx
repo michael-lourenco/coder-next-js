0b12406218872a16c8b113ab0adc16e59d8eab84
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-09-22 10:55:03 -0300

    Componente titulo

--- admin-template/src/components/template/Titulo.tsx
+++ admin-template/src/components/template/Titulo.tsx
@@ -7,11 +7,15 @@ export default function Titulo(props: TituloProps) {
   return (
     <div>
       <h1 className = {`
-      
+        font-black
+        text-3xl
+        text-gray-900
       `}>{ props.titulo }</h1>
       
       <h2 className = {`
-      
+        font-light
+        text-sm
+        text-gray-600
       `}>{ props.subtitulo }</h2>
     </div>
   )
