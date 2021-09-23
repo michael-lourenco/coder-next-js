09268b2c3e59088c79be203b9c4c8c81744aa664
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-09-23 09:49:02 -0300

    Componente logo

--- admin-template/src/components/template/MenuLateral.tsx
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -1,10 +1,24 @@
 import { IconeAjustes, IconeCasa, IconeSino } from '../icons'
+import Logo from './Logo'
 import MenuItem from './MenuItem'
 // Para mais icones acessar... https://heroicons.com
 
 export default function MenuLateral() {
   return(
     <aside>
+      <div className = {`
+        flex
+        flex-col
+        items-center
+        justify-center
+        bg-gradient-to-r
+        from-indigo-500
+        to-purple-800
+        h-20
+        w-20
+      `}>
+        <Logo />
+      </div>
       <ul>
         <MenuItem 
           url = '/'
