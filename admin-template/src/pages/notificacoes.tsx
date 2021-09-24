26bbe04d44c4625a95807ff3ffedc2d64c5b23bd
MichaelLourenco <michael.lourenco@pixter.com.br>
2021-09-23 23:56:38 -0300

    Context API #01

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -3,13 +3,14 @@ import useAppData from '../data/hook/useAppData'
 
 export default function Notificacoes() {
   const dados = useAppData()
+
   return (
     <div>
       <Layout 
         titulo = 'Notificações' 
         subtitulo = 'Gerenciamento de nofificacoes'
       >
-          <h3> { dados.nome }</h3>
+          <h3>{ dados.nome }</h3>
       </Layout>     
     </div>
   )
