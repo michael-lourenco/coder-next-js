8f3971a2f8b3526445b68fbe2531de9c7168a38f
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-04 16:25:29 -0300

    Tela autenticacao #03

--- admin-template/src/pages/autenticacao.tsx
+++ admin-template/src/pages/autenticacao.tsx
@@ -17,14 +17,34 @@ export default function Autenticacao() {
   return(
     <div className={`
       flex
-      flex-col
       h-screen
       items-center
       justify-center
     `}>
-      <div className="w-1/2">
-        <h1 className={`
-          text-xl
+      <div className = {`
+        hidden
+        md:block
+        md:w-1/2
+        lg:w-2/3
+      `}>
+        <img 
+          src = "https://source.unsplash.com/random" 
+          alt = "Imagem da Tela de Autenticação" 
+          className = {`
+            h-screen
+            w-full
+            object-cover
+          `}
+        />
+      </div>
+      <div className = {`
+        m-10 
+        w-full
+        md:w-1/2
+        lg:w-1/3
+      `}>
+        <h1 className = {`
+          text-3xl
           font-bold
           mb-5
         `}>{ modo == 'login' ? 'Entre com sua conta' : 'Cadastre-se na plataforma' }</h1>
@@ -74,6 +94,33 @@ export default function Autenticacao() {
         >
           Entrar com Google
         </button>
+        { modo == 'login' ? (
+          <p className = "mt-8">
+            Novo por aqui?
+            <a 
+              onClick = { () => setModo('cadastro') }
+              className = {`
+                text-blue-500
+                hover:text-blue-700
+                font-semibold
+                cursor-pointer
+              `}
+            > Crie uma conta gratuitamente</a>
+          </p>
+        ) : (
+          <p className = "mt-8">
+            Já faz parte da nossa comunidade?
+            <a 
+              onClick = { () => setModo('login') }
+              className = {`
+                text-blue-500
+                hover:text-blue-700
+                font-semibold
+                cursor-pointer
+              `}
+            > Entre com as suas credenciais</a>
+          </p>
+        )}
       </div>
     </div>
   )
