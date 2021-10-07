b15d75c9b7a265c6a189c0f71ad16437d1d3bf9f
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-06 21:04:03 -0300

    Autenticacao com google

--- admin-template/src/data/context/AuthContext.tsx
+++ admin-template/src/data/context/AuthContext.tsx
@@ -10,25 +10,31 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps>({})
 
-// async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
-//     const token = await usuarioFirebase.getIdToken()
+async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
+    const token = await usuarioFirebase.getIdToken()
 
-//     return { 
-//         uid: usuarioFirebase.uid, 
-//         nome: usuarioFirebase.displayName,
-//         email: usuarioFirebase.email,
-//         token,
-//         provedor: usuarioFirebase.providerData[0].providerId,
-//         imagemUrl: usuarioFirebase.photoURL,
-//     }
-// }
+    return { 
+        uid: usuarioFirebase.uid, 
+        nome: usuarioFirebase.displayName,
+        email: usuarioFirebase.email,
+        token,
+        provedor: usuarioFirebase.providerData[0].providerId,
+        imagemUrl: usuarioFirebase.photoURL,
+    }
+}
 
 export function AuthProvider(props){
     const [usuario, setUsuario] = useState<Usuario>(null)
 
     async function loginGoogle(){
-        console.log('Login do Google')
-        router.push('/')
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+        if(resp.user?.email){
+            const usuario = await usuarioNormalizado(resp.user)
+            setUsuario(usuario)
+            router.push('/')
+        }
     }
 
     return (
