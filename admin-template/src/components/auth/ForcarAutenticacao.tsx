09797d3395901d05b5eae442d6e301bf0abbf965
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-08 10:21:26 -0300

    Protegendo rotas #02

--- admin-template/src/components/auth/ForcarAutenticacao.tsx
+++ admin-template/src/components/auth/ForcarAutenticacao.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import router from 'next/router'
 import loading from '../../../public/images/loading.gif'
@@ -10,6 +11,17 @@ export default function ForcarAutenticacao(props) {
     function renderizarConteudo() { 
         return (
             <>
+                <Head>
+                    <script 
+                        dangerouslySetInnerHTML = {{
+                            __html:`
+                                if(!document.cookie?.includes("admin-template-cod3r-auth")) {
+                                    window.location.href = "/autenticacao"
+                                }
+                            `
+                        }}
+                    />
+                </Head>
                 { props.children }
             </>
         )
