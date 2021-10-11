9764f62e717cc46b08294c33dcf7512c7feff82d
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-11 18:17:16 -0300

    Configurando projeto next-crud

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div className = {`
+      flex
+      h-screen
+      justify-center
+      items-center
+    `}>
+      <span className = {`
+        text-4xl
+      `}>Texto</span>
+    </div>
+  )
+}
