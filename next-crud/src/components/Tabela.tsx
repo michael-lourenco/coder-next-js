a3555f86d03d40b0019197495aad740f6ab18b5d
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-13 14:49:00 -0300

    Componente tabela #02

--- next-crud/src/components/Tabela.tsx
+++ next-crud/src/components/Tabela.tsx
@@ -9,9 +9,9 @@ export default function Tabela(props: TabelaProps){
     function renderizarCabecalho(){
         return (            
             <tr>
-                <th>Código</th>
-                <th>Nome</th>
-                <th>Idade</th>
+                <th className = "text-left p-4" >Código</th>
+                <th className = "text-left p-4" >Nome</th>
+                <th className = "text-left p-4" >Idade</th>
             </tr>
         )
     }
@@ -19,18 +19,30 @@ export default function Tabela(props: TabelaProps){
     function renderizarDados() { 
         return props.clientes?.map((cliente, i) => {
             return (
-                <tr key = { cliente.id }>
-                    <td>{ cliente.id }</td>
-                    <td>{ cliente.nome }</td>
-                    <td>{ cliente.idade }</td>
+                <tr key = { cliente.id }
+                    className = {`
+                        ${i % 2 ===0 ? 'bg-purple-200' : 'bg-purple-100'}
+                    `}>
+                    <td className = "text-left p-4">{ cliente.id }</td>
+                    <td className = "text-left p-4">{ cliente.nome }</td>
+                    <td className = "text-left p-4">{ cliente.idade }</td>
                 </tr>
             )
         })
     }
 
     return (
-        <table>
-            <thead>{ renderizarCabecalho() }</thead>
+        <table className = {`
+            w-full
+            rounded-xl
+            overflow-hidden
+        `}>
+            <thead className = {`
+                text-gray-100
+                bg-gradient-to-r
+                from-purple-500
+                to-purple-800
+            `}>{ renderizarCabecalho() }</thead>
             <tbody>{ renderizarDados() }</tbody>
         </table>
     )
