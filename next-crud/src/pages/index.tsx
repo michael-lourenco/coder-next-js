845010c5060c2bf741c32e1bc6ca01619f3c17cf
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-14 18:14:16 -0300

    Integrando cadastro com firebase

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -3,27 +3,35 @@ import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
 import Formulario from '../components/Formulario'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import ClienteRepositorio from '../core/ClienteRepositorio'
+import ColecaoCliente from '../backend/db/ColecaoCliente'
 
 export default function Home() {
 
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente,setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes,setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 55, '20'),
-    new Cliente('Carlos', 24, '33'),
-    new Cliente('Pedro', 30, '49'),
-  ]
+  useEffect(obterTodos,[])
+
+  function obterTodos() { 
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente:Cliente) {
     setCliente(cliente)
     setVisivel('form')
   }
 
-  function clienteExcluido(cliente:Cliente) {
-    
+  async function clienteExcluido(cliente:Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
   function novoCliente() {
@@ -31,8 +39,9 @@ export default function Home() {
     setVisivel('form')
   }
 
-  function salvarCliente(cliente:Cliente) {
-    setVisivel('tabela')
+  async function salvarCliente(cliente:Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
   }
 
   return (
