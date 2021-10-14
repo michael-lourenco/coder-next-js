958a8878c70209a3adddd64f680da44efc17c119
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-14 16:51:31 -0300

    Integrando tabela e formulario

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -7,6 +7,9 @@ import { useState } from 'react'
 
 export default function Home() {
 
+  const [cliente,setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 55, '20'),
@@ -15,18 +18,22 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente:Cliente) {
-    
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente:Cliente) {
     
   }
 
-  function salvarCliente(cliente:Cliente) {
-  
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente:Cliente) {
+    setVisivel('tabela')
+  }
 
   return (
     <div className = {`
@@ -47,7 +54,7 @@ export default function Home() {
             justify-end
           `}>
             <Botao cor = "green" className = "mb-4"
-              onClick = { () => setVisivel('form') }
+              onClick = { novoCliente }
             >Novo Cliente</Botao>
           </div>
           <Tabela 
@@ -58,7 +65,7 @@ export default function Home() {
           </>
         ) : (
           <Formulario 
-            cliente = { clientes[1] }
+            cliente = { cliente }
             clienteMudou = { salvarCliente }
             cancelado = { () => setVisivel('tabela') }
           />
