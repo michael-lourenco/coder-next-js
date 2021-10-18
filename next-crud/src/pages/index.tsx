629e8925e9fdc9b564b612e9132057dff31541ec
Michael Lourenco <michael.lourenco@pixter.com.br>
2021-10-17 23:20:00 -0300

    Organizando codigo com hooks

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,48 +1,22 @@
 import Botao from '../components/Botao'
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
-import Cliente from '../core/Cliente'
 import Formulario from '../components/Formulario'
-import { useEffect, useState } from 'react'
-import ClienteRepositorio from '../core/ClienteRepositorio'
-import ColecaoCliente from '../backend/db/ColecaoCliente'
+import useClientes from '../hooks/useClientes'
+
 
 export default function Home() {
 
-  const repo: ClienteRepositorio = new ColecaoCliente()
-
-  const [cliente,setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes,setClientes] = useState<Cliente[]>([])
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-  useEffect(obterTodos,[])
-
-  function obterTodos() { 
-    repo.obterTodos().then(clientes => {
-      setClientes(clientes)
-      setVisivel('tabela')
-    })
-  }
-
-  function clienteSelecionado(cliente:Cliente) {
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  async function clienteExcluido(cliente:Cliente) {
-    await repo.excluir(cliente)
-    obterTodos()
-  }
-
-  function novoCliente() {
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  async function salvarCliente(cliente:Cliente) {
-    await repo.salvar(cliente)
-    obterTodos()
-  }
+  const { 
+    cliente, 
+    clientes, 
+    novoCliente, 
+    salvarCliente,
+    selecionarCliente, 
+    excluirCliente,
+    exibirTabela,
+    tabelaVisivel
+  } = useClientes()
 
   return (
     <div className = {`
@@ -56,7 +30,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo = "Cadastro Simples">
-        { visivel === 'tabela' ? (
+        { tabelaVisivel ? (
           <>
           <div className = {`
             flex
@@ -68,15 +42,15 @@ export default function Home() {
           </div>
           <Tabela 
             clientes = { clientes } 
-            clienteSelecionado = { clienteSelecionado }
-            clienteExcluido = { clienteExcluido }
+            clienteSelecionado = { selecionarCliente }
+            clienteExcluido = { excluirCliente }
             ></Tabela>
           </>
         ) : (
           <Formulario 
             cliente = { cliente }
             clienteMudou = { salvarCliente }
-            cancelado = { () => setVisivel('tabela') }
+            cancelado = { exibirTabela }
           />
         )}
       </Layout>
